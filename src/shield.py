f0e60323b95b53a331f02a49ff228bea45c9144d
onabrcom <pouriya.raj@gmail.com>
2025-02-10 12:35:34 +0330

    fix(shield)!: change dataclass to sprite

--- src/shield.py
+++ src/shield.py
@@ -1,28 +1,26 @@
 import secrets
-from dataclasses import dataclass
 
-import pygame.image
+import pygame
 from pygame import Rect
 from pygame.sprite import Sprite
 
 from . import settings
 
 
-@dataclass
 class Shield(Sprite):
-    screen: pygame.Surface
-    shield_image = pygame.transform.scale(
-        pygame.image.load("data/assets/shield/shield.png"),
-        (25, 25),
-    )
-
-    def __post_init__(self) -> None:
-        self.rect: Rect = self.shield_image.get_rect()
-        self.rect.centerx = secrets.randbelow(settings.SCREEN_WIDTH + 1)
+    def __init__(self, screen: pygame.Surface):
+        super().__init__()
+        self.screen = screen
+        self.image = pygame.transform.scale(
+            pygame.image.load("data/assets/shield/shield.png"),
+            (25, 25),
+        )
+        self.rect: Rect = self.image.get_rect()
+        self.rect.centerx = secrets.randbelow(settings.SCREEN_WIDTH - self.rect.width)
         self.rect.top = 0
 
     def update(self):
         self.rect.y += int(settings.HEART_SPEED_FACTOR * settings.DELTA_TIME)
 
     def draw(self):
-        self.screen.blit(self.shield_image, self.rect)
+        self.screen.blit(self.image, self.rect)
