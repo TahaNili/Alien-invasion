e186d5f4c5ed40f7aea96626879c7eb26b06405f
Matin Afzal <contact.matin@yahoo.com>
2025-04-20 19:12:15 +0330

    Remove print test

--- src/heart.py
+++ src/heart.py
@@ -7,7 +7,6 @@ class Heart(Sprite):
     """A class to manage generated hearts."""
 
     def __init__(self, ai_settings, screen):
-        print("Test")
         super(Heart, self).__init__()
 
         self.ai_settings = ai_settings
